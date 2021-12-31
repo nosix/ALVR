acd970e8f4215b05f6a4dc48947bc4ca7dc0e404
nosix <nosix@users.noreply.github.com>
2021-12-31 11:03:40 +0900

    Hiding the error exacerbates the problem, so remove exception handling

--- alvr/experiments/android-lib-no-ovr/alvr-android-lib/src/main/java/io/github/alvr/android/lib/InputBuffer.kt
+++ alvr/experiments/android-lib-no-ovr/alvr-android-lib/src/main/java/io/github/alvr/android/lib/InputBuffer.kt
@@ -11,10 +11,6 @@ class InputBuffer(
     private val codec: MediaCodec,
     private val frameMap: FrameMap
 ) {
-    companion object {
-        private val TAG = InputBuffer::class.simpleName
-    }
-
     init {
         if (!buffer.isDirect) {
             throw IllegalStateException("InputBuffer must be direct.")
@@ -25,22 +21,14 @@ class InputBuffer(
     fun queueConfig() {
         val presentationTimeUs: Long = 0
         val flags: Int = MediaCodec.BUFFER_FLAG_CODEC_CONFIG
-        try {
-            codec.queueInputBuffer(index, 0, buffer.position(), presentationTimeUs, flags)
-        } catch (e: Exception) {
-            Log.w(TAG, "Can't queue config", e)
-        }
+        codec.queueInputBuffer(index, 0, buffer.position(), presentationTimeUs, flags)
     }
 
     @Suppress("unused") // publish to native code
     fun queue(frameIndex: Long) {
         val presentationTimeUs: Long = System.nanoTime() / 1000
         val flags = 0
-        try {
-            codec.queueInputBuffer(index, 0, buffer.position(), presentationTimeUs, flags)
-            frameMap.put(presentationTimeUs, frameIndex)
-        } catch (e: Exception) {
-            Log.w(TAG, "Can't queue", e)
-        }
+        codec.queueInputBuffer(index, 0, buffer.position(), presentationTimeUs, flags)
+        frameMap.put(presentationTimeUs, frameIndex)
     }
 }
